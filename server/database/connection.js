b706fd109783706a2a6059963512bc1d1b7d44bb
DeepanshiNawghare <nawgharedeepanshi@gmail.com>
2023-04-16 22:57:13 +0530

    first commit

---
+++ server/database/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const connectDB = async()=>{
+    const db=mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+console.log("Db connceted suffuly")
+}).catch((err)=>{console.log("err",err.message)});
+}
+
+module.exports = connectDB
